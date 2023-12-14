7b30817f581ccba5565f18916ccabed369485420
Md Sadique Hossain <152019380+sadique21hs@users.noreply.github.com>
2023-12-14 19:52:53 +0600

    Update Login.java

--- src/guiproject/Login.java
+++ src/guiproject/Login.java
@@ -15,7 +15,7 @@ public class Login extends javax.swing.JFrame {
 
    
     @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
